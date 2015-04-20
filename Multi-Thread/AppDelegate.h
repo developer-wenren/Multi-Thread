c08a915006bad3ff9375a50f5cfbda968b23a601
wrcj <wrcj12138@163.com>
2015-04-20 20:46:58 +0800

    Initial Commit

---
+++ Multi-Thread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Multi-Thread
+//
+//  Created by zjsruxxxy3 on 15/4/20.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Multi-Thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multi-Thread
+//
+//  Created by zjsruxxxy3 on 15/4/20.
+//  Copyright (c) 2015年 WR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
